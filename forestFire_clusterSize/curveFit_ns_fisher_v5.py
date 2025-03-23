79281e151ec5ca54e40dd9a88b3c5a4af7012274
knakaji <knakaji@mac.titech.ac.jp>
2025-03-23 20:51:55 +0900

    Fisher

--- forestFire_clusterSize/curveFit_ns_fisher_v5.py
+++ forestFire_clusterSize/curveFit_ns_fisher_v5.py
@@ -6,9 +6,9 @@
 # それに伴う修正を加えた
 
 # 上記コメントにさらにコメント（250321）
-# そもそもFisher指数なので、これはp = p_c = 0.6について行うべきもの
-# ただしns(p)/ns(p_c)についての計算もp_cについて行うと変になる（percolation clusterが抜ける分exp(-s)とならない）ので
-# ns(p = 0.59)などの計算もできるようにしておくべきか
+# そもそもFisher指数なので、これはp=pc=0.6について行うべきもの
+# ただしns(p)/ns(pc)についての計算もpcについて行うと変になる（percolation clusterが抜ける分exp(-cs)とならない）ので
+# ns(p=0.59)などの計算もできるようにしておくべきか
 
 # 250314コメント
 # sやnsをlogで処理する形に変更
@@ -18,29 +18,29 @@
 # 250323コメント
 # プログラム名をcurveFit_ns_fisher_v5.pyに変更
 
-import sys
 import math
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-'''
-How to use
-% python3 curveFit_critical_ns_v3.py args[1] args[2] args[3] args[4]  args[5]
-args[1] ./data/h_list(log)_100x100_p0.6.txt
-args[2] ./data/x_list(log)_100x100_p0.6.txt
-args[3] ./data/y_mean_list(log)_100x100_p0.6.txt
-args[4] ./data/y_std_list(log)_100x100_p0.6.txt 
-args[5] ./data/bw_list(log)_100x100_p0.6.txt 
-'''
-    
-def head_read():
+# header read
+
+def head_read(lx,ly,p,choice):
+
+    pc = 0.6
+    prob = "{:.2f}".format(p)
+
+    if p >= pc:
+        if choice == "w":
+            header_file = "./data/h_list_{0}x{1}_p{2}_w.txt".format(lx,ly,prob)
+        elif choice == "wo":
+            header_file = "./data/h_list_{0}x{1}_p{2}_wo.txt".format(lx,ly,prob)
+    else:
+        header_file = "./data/h_list_{0}x{1}_p{2}.txt".format(lx,ly,prob)
 
     h_list =[]
 
-    input_file = args[1]
-
-    f=open(input_file, 'r')
+    f=open(header_file, 'r')
     datalist = f.readlines()
     for i in range(len(datalist)):
         data = float(datalist[i].rstrip('\n'))
@@ -63,20 +63,37 @@ def file_open(input_file):
     f.close()
     return x_list
 
-def file_read():
+def file_read(lx,ly,p,choice):
 
-    input1 = args[2]
-    input2 = args[3]
-    input3 = args[4]
-    input4 = args[5]
+    pc = 0.6
+    prob = "{:.2f}".format(p)
 
-    x_array = np.array(file_open(input1))
-    y_array = np.array(file_open(input2))
-    yerr_array = np.array(file_open(input3))
-    bw_array = np.array(file_open(input4))
-    dataset = [x_array, y_array, yerr_array, bw_array]
+    if p >= pc:
+        if choice == "w":
+            input1_file = "./data/log_s_list_{0}x{1}_p{2}_w.txt".format(lx,ly,prob)
+            input2_file = "./data/log_ns_mean_list_{0}x{1}_p{2}_w.txt".format(lx,ly,prob)
+            input3_file = "./data/log_ns_std_list_{0}x{1}_p{2}_w.txt".format(lx,ly,prob)
+            input4_file = "./data/bw_list_{0}x{1}_p{2}_w.txt".format(lx,ly,prob)
+        elif choice == "wo":
+            input1_file = "./data/log_s_list_{0}x{1}_p{2}_wo.txt".format(lx,ly,prob)
+            input2_file = "./data/log_ns_mean_list_{0}x{1}_p{2}_wo.txt".format(lx,ly,prob)
+            input3_file = "./data/log_ns_std_list_{0}x{1}_p{2}_wo.txt".format(lx,ly,prob)
+            input4_file = "./data/bw_list_{0}x{1}_p{2}_wo.txt".format(lx,ly,prob)
+    else:
+        input1_file = "./data/log_s_list_{0}x{1}_p{2}.txt".format(lx,ly,prob)
+        input2_file = "./data/log_ns_mean_list_{0}x{1}_p{2}.txt".format(lx,ly,prob)
+        input3_file = "./data/log_ns_std_list_{0}x{1}_p{2}.txt".format(lx,ly,prob)
+        input4_file = "./data/bw_list_{0}x{1}_p{2}.txt".format(lx,ly,prob)
+
+    x_array = np.array(file_open(input1_file))
+    y_array = np.array(file_open(input2_file))
+    yerr_array = np.array(file_open(input3_file))
+    bw_array = np.array(file_open(input4_file))
+    dataset = [ x_array, y_array, yerr_array, bw_array ]
 
     return dataset
+
+# remove unwanted components
 
 '''
 250323コメント・・・以下は過去のコメント
@@ -84,16 +101,16 @@ def file_read():
 RuntimeWarning: divide by zero
 が出て、値が-infになる。それを除去するための関数がremove_inf
 250314コメントとの連動で、読み込まれる
-x_list、y_mean_list
+log_s_list、log_ns_mean_list
 が既にlogされており、
-y_mean_list
+log_ns_mean_list
 には-infが含まれている。また
-y_std_list
+log_ns_std_list
 にはnanが含まれている。
-しかし、y_mean_listで-infになっているところがnanになっているので
+しかし、log_ns_mean_listで-infになっているところがnanになっているので
 同じインデックスの要素をぬけば良いという発想で抜いている
 250323コメント・・・ここから新しいコメント
-計算順序変更のため、y_mean_listやy_std_listにはもはや-infは含まれていない
+計算順序変更のため、log_ns_mean_listやlog_ns_std_listにはもはや-infは含まれていない
 一方、nanは含まれており、それを除去するための関数がremove_nan
 同様にcurve_fitのsigmaに0が含まれているとエラーになるのでremove_zerosも追加
 '''
@@ -162,26 +179,50 @@ def remove_zeros(dataset):
 
     return new_dataset
 
+# curve fitting
+
 def linearFit(x, a, b):     # 既にlogしたデータを読み込むので線形フィットで良い
     return  a*x + b
 
 def fittedArray(x_array, param):
-    fitted_array = [linearFit(num, param[0], param[1]) for num in x_array]
+    fitted_array = np.array([ linearFit(x, param[0], param[1]) for x in x_array ])
     return fitted_array
 
 
 if __name__ == '__main__':
-    args = sys.argv
+    
+    try:
+        lattice_x = int(input('X-Lattice Size (default=200): '))
+    except ValueError:
+        lattice_x = 200
 
-    h_array = head_read()
+    try:
+        lattice_y = int(input('Y-Lattice Size (default=100): '))
+    except ValueError:
+        lattice_y = 100
 
-    lattice_x = int(h_array[0])
-    lattice_y = int(h_array[1])
-    p = h_array[2]
+    try:
+        p = float(input('Probability (default=0.6): '))
+    except ValueError:
+        p = 0.6
+
+    lx = str(lattice_x)
+    ly = str(lattice_y)
+
+    pc = 0.6
+    choice = "wo"
+
+    if p >= pc:
+        try:
+            choice = input('with/without percolation cluster? (w or wo): ')
+        except:
+            choice = "wo"
+
+    h_array = head_read(lx,ly,p,choice)
     num_repeat = int(h_array[3])
     elapsed_time = h_array[4]
 
-    dataset = file_read()
+    dataset = file_read(lx,ly,p,choice)
 
     logHist_x = dataset[0]
     logHist_y = dataset[1]
@@ -189,7 +230,7 @@ if __name__ == '__main__':
     bar_width = dataset[3]
 
     dataLogHist = [logHist_x, logHist_y, logHist_y_std] 
-#    dataLogHist = remove_infs(dataLogHist)
+#    dataLogHist = remove_infs(dataLogHist) # -infが要素にないので不要になった
     dataLogHist = remove_nans(dataLogHist)
     dataLogHist = remove_zeros(dataLogHist)
 
@@ -225,12 +266,19 @@ if __name__ == '__main__':
     ax2.errorbar(dataLogHist[0], dataLogHist[1], yerr = dataLogHist[2], capsize=2, ecolor=u'#1f77b4', fmt='none')
     ax2.plot(dataLogHist[0], logHist_y_fit,  c='blue')
   
-
-
     result_text5 = "$n_{{s}}$($p$) ∝ $s$$^{{{{{0:.3f}}}±{{{1:.3f}}}}}$".format(param[0], paramErr)
     fig.text(0.75, 0.8, result_text5)
 
-    savefile = "./png/clusterSize_critical_fit_{0}x{1}_p{2}.png".format(lattice_x,lattice_y, p)
+    prob = "{:.2f}".format(p)
+
+    if p >= pc:
+        if choice == "w":
+            savefile = "./png/clusterSize_ns_{0}x{1}_p{2}_w_fitted.png".format(lattice_x,lattice_y, prob)
+        elif choice == "wo":
+            savefile = "./png/clusterSize_ns_{0}x{1}_p{2}_wo_fitted.png".format(lattice_x,lattice_y, prob)
+    else:
+        savefile = "./png/clusterSize_ns_{0}x{1}_p{2}_fitted.png".format(lattice_x,lattice_y, prob)
+
     fig.savefig(savefile, dpi=300)
     
     plt.show()
